9b0496a154033c75928d88f01beb2799f2e15a3f
Castellano-Dan <yaizacastellano@icloud.com>
2025-05-24 22:15:03 -0600

    Se renombra el metodo, para una mejor claridad

---
+++ MejoraBancoSimple2T1-master/BancoSimple2T1/VerTransferenciaForms.cs
@@ -0,0 +1,28 @@
+﻿using BancoSimple2T1.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BancoSimple2T1
+{
+    public partial class VerTransferenciaForms : Form
+    {
+        private readonly BancoSimpleContext _context = new BancoSimpleContext();
+        public VerTransferenciaForms()
+        {
+            InitializeComponent();
+            CargarTransacciones();
+        }
+
+        private void CargarTransacciones()
+        {
+            dgvTransferencias.DataSource = con.Transacciones.ToList();
+        }
+    }
+}
